0ef722c0525bb50ed078303773b59a53c1a2f2d7
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-25 17:07:41 +0300

    refactor string values into all methods

---
+++ core/base/enum/route.enum.js
@@ -0,0 +1,11 @@
+const TOKENS = "tokens";
+const USERS = "users";
+const CHECKS = "checks";
+
+const router = {
+  tokens: TOKENS,
+  users: USERS,
+  checks: CHECKS,
+};
+
+module.exports = router;
