33fd070faff2ff1717580669249eef9d92a0064b
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-26 10:00:12 +0300

    initial gui

--- src/router.js
+++ src/router.js
@@ -1,11 +1,22 @@
 const UserModule = require("./modules/user/user.module");
 const TokenModule = require("./modules/token/token.module");
 const CheckModule = require("./modules/check/check.module");
+const handlers = require("./handlers");
 
 const router = {
-  users: UserModule,
-  tokens: TokenModule,
-  checks: CheckModule,
+  "": handlers.index,
+  "account/create": handlers.AccountCreate,
+  "account/edit": handlers.AccountEdit,
+  "account/deleted": handlers.AccountDeleted,
+  "session/create": handlers.sessionCreate,
+  "session/deleted": handlers.sessionDeleted,
+  "checks/all": handlers.checksList,
+  "checks/create": handlers.checksCreate,
+  "checks/edit": handlers.checksEdit,
+  "checks/deleted": handlers.checksDeleted,
+  "api/users": UserModule,
+  "api/tokens": TokenModule,
+  "api/checks": CheckModule,
 };
 
 module.exports = router;
