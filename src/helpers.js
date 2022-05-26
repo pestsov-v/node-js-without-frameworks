03e28e83841f816d3f7bb270025161be88603531
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-26 13:16:58 +0300

    create the different templates and template methods

--- src/helpers.js
+++ src/helpers.js
@@ -1,13 +1,16 @@
 const path = require("path");
 const fs = require("fs");
+const config = require("../config/variables.config");
 
 const helpers = {};
 
-helpers.getTemplate = function (templateName, callback) {
+helpers.getTemplate = function (templateName, data, callback) {
   templateName =
     typeof templateName == "string" && templateName.length > 0
       ? templateName
       : false;
+
+  data = typeof data == "object" && data !== null ? data : {};
 
   if (templateName) {
     const templatesDir = path.join(__dirname, "/../templates/");
@@ -16,7 +19,8 @@ helpers.getTemplate = function (templateName, callback) {
       "utf8",
       function (err, str) {
         if (!err && str && str.length > 0) {
-          callback(false, str);
+          const finalString = helpers.interpolate(str, data);
+          callback(false, finalString);
         } else {
           callback("No tammplate coult not found");
         }
@@ -25,6 +29,47 @@ helpers.getTemplate = function (templateName, callback) {
   } else {
     callback("A valid templateName was not specified");
   }
+};
+
+helpers.addUniversalTemplates = function (str, data, callback) {
+  str = typeof str == "string" && str.length > 0 ? str : "";
+  data = typeof data == "object" && data !== null ? data : {};
+
+  helpers.getTemplate("_header", data, function (err, headerString) {
+    if (!err && headerString) {
+      helpers.getTemplate("_footer", data, function (err, footerString) {
+        if (!err && footerString) {
+          const fullString = headerString + str + footerString;
+          callback(false, fullString);
+        } else {
+          callback("Could not find the footer template");
+        }
+      });
+    } else {
+      callback("Could-not find the header template");
+    }
+  });
+};
+
+helpers.interpolate = function (str, data) {
+  str = typeof str == "string" && str.length > 0 ? str : "";
+  data = typeof data == "object" && data !== null ? data : {};
+
+  for (const keyName in config.templateGlobals) {
+    if (config.templateGlobals.hasOwnProperty(keyName)) {
+      data["global." + keyName] = config.templateGlobals[keyName];
+    }
+  }
+
+  for (const key in data) {
+    if (data.hasOwnProperty(key) && typeof data[key] == "string") {
+      const replace = data[key];
+      const find = "{" + key + "}";
+      str = str.replace(find, replace);
+    }
+  }
+
+  return str;
 };
 
 module.exports = helpers;
