6bcdf6b5cbc403ed31cd3587d75638d389905f1f
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-31 19:17:58 +0300

    refactor gui public methods

---
+++ server/src/core/base/enum/extensionType.js
@@ -0,0 +1,13 @@
+const CSS = ".css";
+const PNG = ".png";
+const JPG = ".jpg";
+const ICO = ".ico";
+
+const extensionType = {
+  css: CSS,
+  png: PNG,
+  jpg: JPG,
+  ico: ICO,
+};
+
+module.exports = extensionType;
